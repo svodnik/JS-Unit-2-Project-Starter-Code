c8e2a1f9009469df7fb14eae84e51cb4194175da
Jules Forrest <julesforrest@gmail.com>
2017-03-15 21:20:36 -0700

    got popup working

--- js/app.js
+++ js/app.js
@@ -3,3 +3,14 @@
   Jules Forrest
   Please add all Javascript code to this file.
 */
+
+$('.article').click(function(){
+  $('#popUp').removeClass('hidden loader');
+  var headline = $(this).find('h3').text();
+  $('#popUp').find('h1').text(headline);
+
+})
+
+$('.closePopUp').click(function(){
+  $('#popUp').addClass('hidden');
+});
